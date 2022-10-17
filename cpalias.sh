ac1cbccdcb5345cbb9e33036a595d4368ef63bbf
vmummer <54071194+vmummer@users.noreply.github.com>
2022-10-17 10:42:04 -0400

    Update cpalias.sh
    Non usable Token removed

--- cpalias.sh
+++ cpalias.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
-export TOKEN=cp-us-e4d7e1ca-c542-4b98-ae6e-ebb4e80d3bf3d1909eb0-de4c-4326-b44c-fe102899578
+export TOKEN= 
 echo "Adding Check Point AppSec Lab Alias Commands.  Use cp_help for list of commands"
 DOCKER_HOST="`hostname -I| awk ' {print $1}'`"
 alias cp_tr_bad='docker run -it --rm -v $(pwd)/data:/home/web-scraper/data --add-host juiceshop.local:$DOCKER_HOST -w /home/juice-shop-solver appsec-demo_test-host python main.py'
