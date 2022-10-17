8bd0f14a8ae684a133b8545307cdb3d50f014222
Vince Mammoliti <vincem@checkpoint.com>
2022-10-17 10:33:31 -0400

    updated alias

--- cp/cp_token.sh
+++ cp/cp_token.sh
@@ -1,21 +1,21 @@
 #/usr/bin/bash
 if [[ $PWD =~ .*appsec-demo/cp ]];then
-        echo "cp_token is required to be run from <....>/appsec-demo root directory"
+        echo -e "cp_token is required to be run from <....>/appsec-demo root directory"
         exit 1
 fi
-if [[ $TOKEN ]]; then
-        echo "TOKEN variable is set to \n TOKEN=$TOKEN"
-        else
-           echo "TOKEN variable is NOT SET!  Please set before continuing with LAB"
-fi
-
 if [ -z "$1" ]; then
         echo "Usage  cp_token         - Display curent TOKEN value" 
-	echo "Usage  cp_token <TOKEN> - Set TOKEN Variable - <Check Point AppSec TOKEN copied from Infinity AppSEC portal>"
+	echo "       cp_token <TOKEN> - Set TOKEN Variable - <Check Point AppSec TOKEN copied from Infinity AppSEC portal>"
         echo ""
-        exit 1
+        if [[ $TOKEN ]]; then
+	       echo -e "TOKEN variable is set to: $TOKEN \n"
+	       exit 1
+	       else
+	          echo "TOKEN variable is NOT SET!  Please set before continuing with LAB"
+	fi
+        exit 1	
 else
         sed -i 's/TOKEN=.*/TOKEN='$1'/' cpalias.sh
         export TOKEN=$1
-        echo "TOKEN varable is set to:  TOKEN=$TOKEN, and value is updated in cpalias.sh script"
+        echo -e "TOKEN varable is changed to: \n $TOKEN in cpalias.sh script.\n Important reload the file via issuing the following command: \n source cpalias.sh \n for variable to be set. "
 fi
--- cpalias.sh
+++ cpalias.sh
@@ -1,22 +1,21 @@
 #/usr/bin/bash
-shopt -s expand_aliases
+export TOKEN=cp-us-e4d7e1ca-c542-4b98-ae6e-ebb4e80d3bf3d1909eb0-de4c-4326-b44c-fe102899578
 echo "Adding Check Point AppSec Lab Alias Commands.  Use cp_help for list of commands"
-chmod +x cp/*.sh
-export TOKEN=cp-us-e4d7e1ca-c542-4b98-ae6e-ebb4e80d3bf3d1909eb0-de4c-4326-b44c-fe1028995780
 DOCKER_HOST="`hostname -I| awk ' {print $1}'`"
 alias cp_tr_bad='docker run -it --rm -v $(pwd)/data:/home/web-scraper/data --add-host juiceshop.local:$DOCKER_HOST -w /home/juice-shop-solver appsec-demo_test-host python main.py'
 alias cp_tr_good='docker run -it --rm -v $(pwd)/data:/home/web-scraper/data --add-host juiceshop.local:$DOCKER_HOST appsec-demo_test-host python /home/web-scraper/websitescrap.py'
 alias cp_cpnano='docker exec -it cp-appsec-agent-container /usr/sbin/cpnano'
 alias cp_uninstall='docker exec -it cp-appsec-agent-container /usr/sbin/cpnano --uninstall'
-alias cp_agent_token='docker exec -it cp-appsec-agent-container ./cp-nano-agent --token' $TOKEN
+alias cp_agent_token='docker exec -it cp-appsec-agent-container ./cp-nano-agent --token $TOKEN'
 alias cp_token="bash cp/cp_token.sh"
-alias cp_cpnanos='docker exec -it cp-appsec-agent-container /usr/sbin/cpnano -s |grep status'
+alias cp_cpnanol='docker exec -it cp-appsec-agent-container /usr/sbin/cpnano -s |grep Last' 
 alias cp_wipe='docker-compose down &&  docker system prune -a'
 alias cp_cert='sh cp/cp_get_cert.sh'
 alias cp_fetch='git  config --global http.sslverify false && git clone https://github.com/vmummer/appsec-demo.git'
 alias cp_host='printf "Docker Host IP address used: $DOCKER_HOST \n"'
 alias cp_help='printf "Check Point Lab Commands:\n
-cp_cpnano      Show detail  status of AppSec Agent (cp_cpnano -s)
+cp_cpnano      Show detail status of AppSec Agent ( use as cp_cpnano -s)
+cp_cpnanol     Show last update of the AppSec Agent
 cp_uninstall   Uninstall AppSec Agent
 cp_agent_token Install AppSec Agent and assign Token
 cp_token       Display and update AppSec Agent Token Variable
