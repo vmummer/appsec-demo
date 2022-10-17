d6b7929af43c2fbf3ae4c5be95d48f4921ccb31c
Vince Mammoliti <vincem@checkpoint.com>
2022-10-17 16:03:28 -0400

    fixed typo

--- cpalias.sh
+++ cpalias.sh
@@ -1,6 +1,6 @@
 #/usr/bin/bash
-export TOKEN=cp-us-e4d7e1ca-c542-4b98-ae6e-ebb4e80d3bf3d1909eb0-de4c-4326-b44c-fe102899578
-echo "Adding Check Point AppSec Lab Alias Commands.  Use cp_help for list of commands"
+export TOKEN=
+echo "Adding Check Point AppSec Lab Alias Commands.  Use cphelp for list of commands"
 DOCKER_HOST="`hostname -I| awk ' {print $1}'`"
 alias cptrbad='docker run -it --rm -v $(pwd)/data:/home/web-scraper/data --add-host juiceshop.local:$DOCKER_HOST -w /home/juice-shop-solver appsec-demo_test-host python main.py'
 alias cptrgood='docker run -it --rm -v $(pwd)/data:/home/web-scraper/data --add-host juiceshop.local:$DOCKER_HOST appsec-demo_test-host python /home/web-scraper/websitescrap.py'
